d0c32092c0745b41e5aca82cf2389e8f7b089e57
InfoDesigner <101991676+InfoDesigner@users.noreply.github.com>
2026-07-02 18:20:00 -0700

    Noqa adjustment

--- src/xleda/__init__.py
+++ src/xleda/__init__.py
@@ -4,7 +4,7 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from .main import wb
+from .main import wb  # noqa: E402
 
 
 __all__ = ['wb']
